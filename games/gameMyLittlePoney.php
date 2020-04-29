380554908fa34d8a2b570422e3563908eeb1ac57
vara1991 <varaponegaire@gmail.com>
2020-04-29 18:23:46 -0400

    add mylittleponey

--- games/gameMyLittlePoney.php
+++ games/gameMyLittlePoney.php
@@ -1,1 +1,30 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+        <link rel="stylesheet" href="../assets/css/myLittlePoney.css"
+    </head>
+
+    <body class="bg-black-poney">
+
+            <div id="poneygame-conteneur">
+                <p> <span class="go-right-poney"> <- : go right </span> | <span class="go-left-poney"> -> : go left </span></p>
+            </div>
+
+            <canvas id="myCanvas" width="500" height="450"></canvas>
+
+
+            <button class="ponney-button" type="button" onclick=window.location.href="">Next world</button>
+
+            <button class="ponney-button" type="button" onclick=window.location.href="../pages/planetMyLittlePoney.php">The world of my little pony</button>
+
+        <script type="text/javascript" src="../assets/js/myLittlePoney.js"></script>
+    </body>
+</html>
+
+
