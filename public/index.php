2a35048884562911b2b34edf767fd64ebf4a14b1
StenQuidelleur <sten.quidelleur@outlook.fr>
2020-04-30 09:56:49 +0200

    Pofiniolage

--- public/index.php
+++ public/index.php
@@ -6,17 +6,36 @@ include '../layouts/head.php';
 </audio>
 <div class="bg-index">
     <div class="container">
-       <div class="row">
-            <div class="jumbotron bg-dark col-8 cont-index">
+        <div class="row justify-content-space-between">
+           <div>
+               <p>
+                   <a class="btn btn-dark games mt-2 " data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                       Games
+                   </a>
+               </p>
+               <div class="collapse" id="collapseExample">
+                   <ul class="list-group ">
+                       <li class="list-group-item bg-dark"><a href="../games/gameAmityVille.php">AmityVille</a></li>
+                       <li class="list-group-item bg-dark"><a href="../games/gameAsile.php.php">Asile</a></li>
+                       <li class="list-group-item bg-dark"><a href="../games/gameGameOver.php.php">GameOver</a></li>
+                       <li class="list-group-item bg-dark"><a href="../games/gameJurassicPark.php.php">JurassicPark</a></li>
+                       <li class="list-group-item bg-dark"><a href="../games/gameMirror.php.php">Mirror</a></li>
+                       <li class="list-group-item bg-dark"><a href="../games/gameMyLittlePoney.php.php">MyLittlePoney</a></li>
+                       <li class="list-group-item bg-dark"><a href="../games/gamePokemon.php.php">Pokemon</a></li>
+                   </ul>
+               </div>
+           </div>
+
+            <div class="jumbotron bg-dark col-8 ml-2 cont-index">
                 <h1 class="display-4 font-weight-bold mb-5">Hey, Dude!</h1>
-                <p class="lead font-weight-bold">You are tired of being a vegetable and you want to live crazy adventures?</p>
+                <p class="lead font-weight-bold">You are stuck in a psychiatric hospital since 3 years now, and it’s time to escape and live the most incredible adventure of your life !</p>
                 <hr class="my-4">
-                <p class="font-weight-bold">Do like Johnny, turn off the light, press Go and go out the window !</p>
+                <p class="font-weight-bold">What are you waiting for ? Just click GO and escape !</p>
                 <p class="lead">
-                    <a class="btn btn-danger btn-lg mt-4 " href="../games/gameAmityVille.php" role="button">GO !</a>
+                    <a class="btn btn-danger btn-lg mt-4 go " href="../games/gameAmityVille.php" role="button">GO !</a>
                 </p>
             </div>
-       </div>
+        </div>
     </div>
 </div>
 
